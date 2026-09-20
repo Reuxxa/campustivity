f13d61e0bf4a5d986f9ee79614e82d9714a85a8d
Reuxxa <tarisadwiananda573@gmail.com>
2026-09-21 01:10:46 +0700

    Configure TiDB cloud database and add missing dependencies

--- koneksi.py
+++ koneksi.py
@@ -3,13 +3,25 @@ from mysql.connector import Error
 
 def connect_db():
     try:
+        # Coba koneksi ke TiDB Cloud Database
         connection = mysql.connector.connect(
-            host='localhost',       
-            user='root',            
-            password='',            
-            database='sistemkegiatan'  
+            host='gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+            port=4000,
+            user='3w7FvA6qK4YqS8b.root',
+            password='LKSFCgi4QLf8GPUq',
+            database='sistemkegiatan'
         )
         return connection
     except Error as e:
-        print(f"Error saat koneksi ke MySQL: {e}")
-        return None
+        # Fallback ke localhost jika offline / lokal
+        try:
+            connection = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='',
+                database='sistemkegiatan'
+            )
+            return connection
+        except Error:
+            print(f"Error saat koneksi ke MySQL: {e}")
+            return None
